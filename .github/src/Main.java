9a9e8acc254fb5ad5d4b41315c8dc4daa7836a98
zsmlnr <zsozsomlnr.96@gmail.com>
2021-02-11 14:27:29 +0100

    Add exerciseFinalProduct

--- .github/src/Main.java
+++ .github/src/Main.java
@@ -1,18 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        String firstWorld="Hello World";
+        String firstWorld=" Hello World ";
 
-        System.out.println(firstWorld+" "+firstWorld.length());
+        System.out.println(firstWorld+firstWorld.length());
         System.out.println(firstWorld.toLowerCase());
         System.out.println(firstWorld.toUpperCase());
         System.out.println(firstWorld.replaceFirst("World","CodersBay"));
-
-        for (int i=0; i<=14; i++){
-                System.out.println(firstWorld);
-            }
-
-
-
+        System.out.println((firstWorld+"\n").repeat(15));
+        System.out.println(firstWorld.stripLeading());
     }
 }
