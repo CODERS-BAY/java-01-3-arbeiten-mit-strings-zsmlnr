dcca8368282a6f1e3f1bed08f493d13572fe6bf0
zsmlnr <zsozsomlnr.96@gmail.com>
2021-02-11 13:58:29 +0100

    Add exercise

---
+++ .github/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String firstWorld="Hello World";
+
+        System.out.println(firstWorld+" "+firstWorld.length());
+        System.out.println(firstWorld.toLowerCase());
+        System.out.println(firstWorld.toUpperCase());
+        System.out.println(firstWorld.replaceFirst("World","CodersBay"));
+
+        for (int i=0; i<=14; i++){
+                System.out.println(firstWorld);
+            }
+
+
+
+    }
+}
